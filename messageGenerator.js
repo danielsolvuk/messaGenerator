acff724f6bef5b58f87cd457d688af5555494ff3
danielsolvik <danielsolvik@yahoo.com>
2023-03-19 20:54:17 +0100

    Added random words to display

--- messageGenerator.js
+++ messageGenerator.js
@@ -1,1 +1,5 @@
-console.log("hello!")
+const randomMessages = ['You are so hot', 'Programming is fun', 'Football is the best sport']
+
+const word = randomMessages[Math.floor(Math.random() * randomMessages.length)]
+
+console.log(word)
